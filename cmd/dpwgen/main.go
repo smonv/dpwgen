983c1b2861b4da7a177e3329145ea421c970dbbf
Thanh Van <tthanh.smt@gmail.com>
2018-01-29 14:12:30 +0700

    add basic random password

--- cmd/dpwgen/main.go
+++ cmd/dpwgen/main.go
@@ -1,13 +1,32 @@
 package main
 
 import (
+	"crypto/rand"
+	"flag"
 	"fmt"
+	"math/big"
+	"strconv"
 
 	"github.com/tthanh/dpwgen/wordlist"
 )
 
 func main() {
-	for _, s := range wordlist.EFFLarge {
-		fmt.Println(s)
+	n := flag.Int("n", 6, "number of words")
+	flag.Parse()
+	words := []string{}
+	for w := 0; w < *n; w++ {
+		k := ""
+		for i := 0; i < 5; i++ {
+			m, err := rand.Int(rand.Reader, big.NewInt(int64(6)))
+			if err != nil {
+				panic(err)
+			}
+			k += strconv.Itoa(int(m.Int64()) + 1)
+		}
+		words = append(words, k)
+	}
+
+	for _, v := range words {
+		fmt.Printf("%s - %s\n", v, wordlist.EFFLarge[v])
 	}
 }
